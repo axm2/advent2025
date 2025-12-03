6649740c86dc8ae165c6e6c1223c5ea987f84834
Omar Mirza <24306299+axm2@users.noreply.github.com>
2025-12-03 17:11:41 -0500

    3b

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 advent_of_code::solution!(3);
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -20,7 +21,31 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut total = 0;
+    for bank in input.lines(){
+        let mut v = vec![0u32; 12];
+        // for every digit, we check how many digits are left to read.
+        // If there are n digits left to read, we start at v[12-n] and see if the digit is larger than the stored one.
+        // If so, we place it there and set the ones to the right to zero.
+        // If not, we check the next position to the right.
+        for (i,n) in bank.chars().filter_map(|c| c.to_digit(10)).enumerate(){
+            let digits_left = cmp::min(bank.len() - i, 12);
+            for j in (12 - digits_left)..12 {
+                if n > v[j] {
+                    v[j] = n;
+                    for k in (j+1)..12 {
+                        v[k] = 0;
+                    }
+                    break;
+                }
+            }
+
+        }
+        // turn the vector into a number by turning it into a string first
+        let intermediate: u64 = v.iter().map(|d| d.to_string()).collect::<String>().parse().unwrap();
+        total+=intermediate;
+    }
+    return Some(total);
 }
 
 #[cfg(test)]
@@ -31,6 +56,12 @@ mod tests {
     fn test_example_part_one() {
         let result = part_one(&advent_of_code::template::read_file_part("examples", DAY,0));
         assert_eq!(result, Some(357));
+    }
+
+    #[test]
+    fn test_example_part_two() {
+        let result = part_two(&advent_of_code::template::read_file_part("examples", DAY,0));
+        assert_eq!(result, Some(3121910778619));
     }
 
     #[test]
