020452a69e2ae3ffc2726c0779097baf62c2955f
Omar Mirza <24306299+axm2@users.noreply.github.com>
2025-12-03 00:55:54 -0500

    3a

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -3,17 +3,17 @@ advent_of_code::solution!(3);
 pub fn part_one(input: &str) -> Option<u32> {
     let mut total = 0;
     for bank in input.lines(){
-        let mut h_1 = 0;
-        let mut h_2 = 0;
+        // let mut v = vec![0u32; 2];
+        let mut a = [0u32; 2];
         for n in bank.chars().filter_map(|c| c.to_digit(10)){
-            if h_1 < h_2 {
-                std::mem::swap(&mut h_1, &mut h_2);
-            }
-            if n > h_2 {
-                h_2 = n;
+            if a[0]*10 + a[1] < a[1]*10 + n {
+                a[0] = a[1];
+                a[1] = n;
+            } else if a[1]<n{
+                a[1] = n;
             }
         }
-        let intermediate = h_1*10+h_2;
+        let intermediate= a[0]*10+a[1];
         total+=intermediate;
     }
     return Some(total);
@@ -30,13 +30,13 @@ mod tests {
     #[test]
     fn test_example_part_one() {
         let result = part_one(&advent_of_code::template::read_file_part("examples", DAY,0));
-        assert_eq!(result, Some(381));
+        assert_eq!(result, Some(357));
     }
 
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(17092));
     }
 
     #[test]
